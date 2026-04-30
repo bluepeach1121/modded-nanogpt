5ff329177d4aeee19885a4dd860c675d1f70054a
Bluepeach1121 <olawalefortune1121@gmail.com>
2026-04-30 02:22:27 -0700

    muon2

--- records/track_3_optimization/muon2train_gpt_simple.py
+++ records/track_3_optimization/muon2train_gpt_simple.py
@@ -269,7 +269,7 @@ model.compile(dynamic=False)
 ########################################
 
 # we want to minimize this while still reaching 3.28 val loss
-train_steps = 3500
+train_steps = 50
 
 # initialize model parameters
 for name, p in model.named_parameters():
